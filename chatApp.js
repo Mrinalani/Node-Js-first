b55891d17616302ce17cd7d1828760b601f1c326
pandeymrinalani <PAndeymrinalali02@gmail.com>
2023-07-08 15:29:03 +0530

    filterPath

--- chatApp.js
+++ chatApp.js
@@ -1,31 +1,21 @@
 
-
 const express = require('express');
-const bodyParser = require('body-parser');
+ const bodyParser = require('body-parser');
 
 const app = express();
+const adminroutes = require('./routes/admin')
+const shoproutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('in another middleware');
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title" placeholder="Product Title">
-      <input type="text" name="size" placeholder="Product Size">
-      <button type="submit">Add Product</button>
-    </form>
-  `);
-});
+app.use('/admin',adminroutes)
+app.use(shoproutes)
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body); 
-  res.redirect('/');
-});
+app.use((req, res, next)=>{
+res.status(404).send('<h1>Page not found</h1>')
+})
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>hello from express!</h1>');
-});
+
 
 app.listen(6002);
 
